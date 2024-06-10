f639a2e6618932b122d7bab345b0e09df8113a4d
Kamil097 <kamilrojek2001@gmail.com>
2024-06-10 20:07:53 +0200

    początki prac nad zmianą koloru

--- thoughtsApp/Tui/BufferEditor.cs
+++ thoughtsApp/Tui/BufferEditor.cs
@@ -5,101 +5,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Xml.Serialization;
+using System.Drawing;
+
 namespace thoughtsApp.Tui
 {
     public class BufferEditor
     {
-        public int left { get; set; }
-        public int top { get; set; }
-        public string input { get; set; }
-        public string header { get; set; }  
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public string Input { get; set; }
+        public string Header { get; set; }
+        public ColorEnum CurrentColor { get; set; } = ColorEnum.Default;
+        public enum ColorEnum {Default=1, Red=2, Yellow=3, Blue=4, Purple=5, Green=6}
+        public bool IsFormattingActive { get; set; } = false;
+        public int EnumLength => Enum.GetValues(typeof(ColorEnum)).Length;  
         public BufferEditor(string inputText, string header)
         {
-            this.input = inputText;
-            this.header = header;
+            this.Input = inputText;
+            this.Header = header;
         }
 
         public string Run()
         {
             var maxheight = WindowHeight;
             SetCursorPosition(0, CursorTop);
-            StringBuilder editedText = new StringBuilder(input.Trim());
+            StringBuilder editedText = new StringBuilder(Input.Trim());
             ConsoleKeyInfo key;
-            WriteLine(header);
+            WriteLine(Header);
             Write(editedText);
 
             do
             {
-                left = GetCursorPosition().Left;
-                top = GetCursorPosition().Top;
-                (int maxRow,int maxCol) maxIndex = GetMaxIndex(editedText.ToString());
-                SetCursorPosition(left, top);
+                Left = GetCursorPosition().Left;
+                Top = GetCursorPosition().Top;
+                (int maxRow, int maxCol) maxIndex = GetMaxIndex(editedText.ToString());
+                SetCursorPosition(Left, Top);
                 key = ReadKey();
 
                 if (key.Key == ConsoleKey.Backspace && editedText.Length > 0)
                 {
-                    editedText.Remove(left + (top-1) * WindowWidth - 1, 1);
+                    editedText.Remove(Left + (Top - 1) * WindowWidth - 1, 1);
                     Clear();
-                    WriteLine(header);
+                    WriteLine(Header);
                     SetCursorPosition(0, CursorTop);
                     Write(editedText.ToString());
-                    if (left == 0)
-                        SetCursorPosition(WindowWidth - 1, top - 1);
+                    if (Left == 0)
+                        SetCursorPosition(WindowWidth - 1, Top - 1);
                     else
-                        SetCursorPosition(left - 1, top);
+                        SetCursorPosition(Left - 1, Top);
 
                 }
-                else if (key.Key == ConsoleKey.RightArrow && (top < maxIndex.maxRow || maxIndex.maxCol > left - 1))
+                else if (key.Key == ConsoleKey.RightArrow && (Top < maxIndex.maxRow || maxIndex.maxCol > Left - 1))
                 {
-                    if (WindowWidth - 1 == left)
-                        SetCursorPosition(0, top + 1);
+                    if (WindowWidth - 1 == Left)
+                        SetCursorPosition(0, Top + 1);
                     else
-                        SetCursorPosition(left + 1, top);
+                        SetCursorPosition(Left + 1, Top);
                 }
-                else if (key.Key == ConsoleKey.LeftArrow && left > 0)
+                else if (key.Key == ConsoleKey.LeftArrow && Left > 0)
                 {
-                    SetCursorPosition(left - 1, top);
+                    SetCursorPosition(Left - 1, Top);
                 }
-                else if (key.Key == ConsoleKey.LeftArrow && top > 1)
+                else if (key.Key == ConsoleKey.LeftArrow && Top > 1)
                 {
-                    SetCursorPosition(WindowWidth - 1, top - 1);
+                    SetCursorPosition(WindowWidth - 1, Top - 1);
                 }
-                else if (key.Key == ConsoleKey.DownArrow && top < maxIndex.maxRow)
+                else if (key.Key == ConsoleKey.DownArrow && Top < maxIndex.maxRow)
                 {
-                    if (top != maxIndex.maxRow - 1 || left <= maxIndex.maxCol + 1)
-                        SetCursorPosition(left, top + 1);
+                    if (Top != maxIndex.maxRow - 1 || Left <= maxIndex.maxCol + 1)
+                        SetCursorPosition(Left, Top + 1);
                 }
-                else if (key.Key == ConsoleKey.UpArrow && top > 1)
+                else if (key.Key == ConsoleKey.UpArrow && Top > 1)
                 {
-                    SetCursorPosition(left, top - 1);
+                    SetCursorPosition(Left, Top - 1);
                 }
                 else if (key.Key == ConsoleKey.Escape)
                     return "";
+                else if(key.Modifiers.HasFlag(ConsoleModifiers.Alt) && key.Key == ConsoleKey.V)
+                {
+                    CurrentColor = GetNextColor();
+                }
                 else if (!char.IsControl(key.KeyChar))
                 {
+                    UpdateColor();
+                    editedText.Insert(Left + (Top - 1) * WindowWidth, key.KeyChar);
 
-                    editedText.Insert(left + (top-1) * WindowWidth, key.KeyChar);
+                    if (Left == WindowWidth - 1)
+                        SetCursorPosition(0, Top + 1);
 
-                    if (left == WindowWidth - 1)
-                        SetCursorPosition(0, top + 1);
-
-                    if (left + (top-1) * WindowWidth < editedText.Length - 1)
+                    if (Left + (Top - 1) * WindowWidth < editedText.Length - 1)
                     {
                         Clear();
-                        WriteLine(header);
+                        WriteLine(Header);
                         SetCursorPosition(0, CursorTop);
                         Write(editedText.ToString());
-                        if (WindowWidth - 1 == left)
-                            SetCursorPosition(0, top + 1);
+                        if (WindowWidth - 1 == Left)
+                            SetCursorPosition(0, Top + 1);
                         else
-                            SetCursorPosition(left + 1, top);
+                            SetCursorPosition(Left + 1, Top);
                     }
                 }
-                
-                
+
+
             }
             while (key.Key != ConsoleKey.Enter);
-            return editedText.ToString();   
+            return editedText.ToString();
         }
         private static (int maxRow, int maxCol) GetMaxIndex(string editText)
         {
@@ -110,10 +123,38 @@ namespace thoughtsApp.Tui
                 editTextlen -= WindowWidth;
                 result.maxRow++;
             }
-            if(editTextlen > 0)
+            if (editTextlen > 0)
                 result.maxRow++;
-            result.maxCol = editTextlen-1;
+            result.maxCol = editTextlen - 1;
             return result;
+        }
+        public ColorEnum GetNextColor() {
+            var index = (int)CurrentColor;
+            int nextValue = index == EnumLength ? 1 : index + 1;
+            IsFormattingActive = true;
+            return (ColorEnum)nextValue;
+        }
+        public void UpdateColor() {
+            switch (CurrentColor) {
+                case ColorEnum.Default:
+                    ForegroundColor = ConsoleColor.White;
+                    break;
+                case ColorEnum.Red:
+                    ForegroundColor = ConsoleColor.Red;  
+                    break;
+                case ColorEnum.Yellow:
+                    ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case ColorEnum.Blue:
+                    ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case ColorEnum.Purple:
+                    ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case ColorEnum.Green:
+                    ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
         }
     }
 }
